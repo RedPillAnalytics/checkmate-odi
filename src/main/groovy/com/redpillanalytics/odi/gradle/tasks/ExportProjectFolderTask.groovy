8702c4da0502b121afd3d41d9e758654993c490b
josecsotomorales <josecarlos271091@gmail.com>
2018-09-12 18:55:54 -0300

    Creating the logic for Export All the Code from a Project folder

--- src/main/groovy/com/redpillanalytics/odi/gradle/tasks/ExportProjectFolderTask.groovy
+++ src/main/groovy/com/redpillanalytics/odi/gradle/tasks/ExportProjectFolderTask.groovy
@@ -2,9 +2,19 @@ package com.redpillanalytics.odi.gradle.tasks
 
 import com.redpillanalytics.odi.Instance
 import groovy.util.logging.Slf4j
+import oracle.odi.core.persistence.transaction.support.DefaultTransactionDefinition
+import oracle.odi.domain.mapping.Mapping
+import oracle.odi.domain.mapping.finder.IMappingFinder
+import oracle.odi.domain.project.OdiFolder
+import oracle.odi.domain.project.OdiPackage
 import oracle.odi.domain.project.OdiProject
+import oracle.odi.domain.project.OdiUserProcedure
+import oracle.odi.domain.project.finder.IOdiFolderFinder
+import oracle.odi.domain.project.finder.IOdiPackageFinder
 import oracle.odi.domain.project.finder.IOdiProjectFinder
+import oracle.odi.domain.project.finder.IOdiUserProcedureFinder
 import oracle.odi.impexp.EncodingOptions
+import oracle.odi.impexp.smartie.ISmartExportService
 import oracle.odi.impexp.smartie.ISmartExportable
 import oracle.odi.impexp.smartie.impl.SmartExportServiceImpl
 import org.gradle.api.DefaultTask
@@ -12,6 +22,8 @@ import org.gradle.api.tasks.Input
 import org.gradle.api.tasks.OutputDirectory
 import org.gradle.api.tasks.TaskAction
 import org.gradle.api.tasks.options.Option
+
+import static oracle.odi.domain.project.OdiFolder.*
 
 @Slf4j
 class ExportProjectFolderTask extends DefaultTask {
@@ -61,6 +73,16 @@ class ExportProjectFolderTask extends DefaultTask {
             description = "The project name to export. Defaults to either 'projectName' or the subdirectory name in SCM.")
     String pname
 
+    @Input
+    @Option(option = "targetProject",
+            description = "The project name to export. Defaults to either 'projectName' or the subdirectory name in SCM.")
+    String targetProject
+
+    @Input
+    @Option(option = "targetFolder",
+            description = "The project name to export. Defaults to either 'projectName' or the subdirectory name in SCM.")
+    String targetFolder
+
     // setSourceBase is not used, but I added it to support Gradle Incremental Build support
     @OutputDirectory
     def getSourceBase() {
@@ -73,8 +95,63 @@ class ExportProjectFolderTask extends DefaultTask {
 
         def instance = new Instance(url, driver, master, work, masterPass, odi, odiPass)
         def exportService = new SmartExportServiceImpl(instance.odi)
-        def encdOption = new EncodingOptions("1.0", "ISO8859_9",  "ISO-8859-9")
 
+        // create the export list
+        List<ISmartExportable> smartExportList = new LinkedList<ISmartExportable> ();
+
+        // create transaction items
+        def tm = instance.getTransactionManager();
+        def tme = instance.getTransactionalEntityManager();
+
+        // create finders
+        def pf = (IOdiProjectFinder)tme.getFinder(OdiProject.class);  // project
+        def ff = (IOdiFolderFinder)tme.getFinder(OdiFolder.class);   // project code folders
+        def mf = (IMappingFinder)tme.getFinder(Mapping.class);
+        def pkf= (IOdiPackageFinder)tme.getFinder(OdiPackage.class);
+        def prf= (IOdiUserProcedureFinder)tme.getFinder(OdiUserProcedure.class);
+
+        // Validate project and folder
+
+        def project = pf.findByCode(targetProject);
+        if (project == null) {
+            println("Project "+ targetProject +" not found")
+        }
+        else //
+        {
+            def folderColl = ff.findByName(targetFolder, targetProject);
+            if (folderColl.size() == 1)
+                folder = folderColl.iterator().next();
+// list the mappings
+            mappingColl = mf.findByProject(targetProject,targetFolder)
+            for (Mapping mapping : mappingColl) {
+                smartExportList.add( (ISmartExportable) mapping); // add the item to the list
+                println(mapping.getName());
+            }
+// list the packages
+            packageColl = pkf.findByProject(targetProject,targetFolder)
+            for (OdiPackage thePackage : packageColl) {
+                smartExportList.add( (ISmartExportable) thePackage);
+                println(thePackage.getName());
+            }
+// list the procedures
+            procedureColl = prf.findByProject(targetProject,targetFolder)
+            for (OdiUserProcedure theProcedure : procedureColl) {
+                smartExportList.add( (ISmartExportable) theProcedure);
+                println(theProcedure.getName());
+            }
+            // list the load plans
+
+
+            // we have a list! So do the export
+            ISmartExportService smartExport = new SmartExportServiceImpl(odiInstance);
+            EncodingOptions encdOption = new EncodingOptions("1.0", "ISO8859_9",  "ISO-8859-9");
+            // wrap in a transaction
+            txnDef = new DefaultTransactionDefinition();
+            tm = odiInstance.getTransactionManager()
+            txnStatus = tm.getTransaction(txnDef)
+            smartExport.exportToXml (smartExportList, codeExportFolder, codeExportFile, true, false, encdOption, false, null, null, true);
+            tm.rollback(txnStatus) // we don't need to persist!
+        }
 
         instance.beginTxn()
 
