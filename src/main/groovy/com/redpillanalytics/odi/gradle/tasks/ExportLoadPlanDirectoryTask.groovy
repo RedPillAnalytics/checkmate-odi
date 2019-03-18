ef969389fad0a7dba47b151bd0ceede9873e5e95
josecsotomorales <josecarlos271091@gmail.com>
2019-03-18 17:07:41 -0300

    Removed unnecessary import

--- src/main/groovy/com/redpillanalytics/odi/gradle/tasks/ExportLoadPlanDirectoryTask.groovy
+++ src/main/groovy/com/redpillanalytics/odi/gradle/tasks/ExportLoadPlanDirectoryTask.groovy
@@ -2,7 +2,6 @@ package com.redpillanalytics.odi.gradle.tasks
 
 import com.redpillanalytics.odi.Instance
 import groovy.util.logging.Slf4j
-import oracle.odi.domain.runtime.loadplan.OdiLoadPlan
 import org.gradle.api.tasks.Internal
 import org.gradle.api.tasks.TaskAction
 
