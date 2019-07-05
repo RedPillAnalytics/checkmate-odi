e7bb13f32a8040b7423651af3566033063b975e8
josesoto <jose.soto@redpillanalytics.com>
2019-07-05 15:03:37 -0300

    Added IFinders to the Instance class

--- src/main/groovy/com/redpillanalytics/odi/odi/Instance.groovy
+++ src/main/groovy/com/redpillanalytics/odi/odi/Instance.groovy
@@ -19,17 +19,39 @@ import oracle.odi.domain.model.OdiModelFolder
 import oracle.odi.domain.model.finder.IOdiModelFinder
 import oracle.odi.domain.model.finder.IOdiModelFolderFinder
 import oracle.odi.domain.project.OdiFolder
+import oracle.odi.domain.project.OdiKM
 import oracle.odi.domain.project.OdiPackage
 import oracle.odi.domain.project.OdiProject
+import oracle.odi.domain.project.OdiSequence
+import oracle.odi.domain.project.OdiUserFunction
 import oracle.odi.domain.project.OdiUserProcedure
+import oracle.odi.domain.project.OdiVariable
 import oracle.odi.domain.project.finder.IOdiFolderFinder
+import oracle.odi.domain.project.finder.IOdiKMFinder
 import oracle.odi.domain.project.finder.IOdiPackageFinder
 import oracle.odi.domain.project.finder.IOdiProjectFinder
+import oracle.odi.domain.project.finder.IOdiSequenceFinder
+import oracle.odi.domain.project.finder.IOdiUserFunctionFinder
 import oracle.odi.domain.project.finder.IOdiUserProcedureFinder
+import oracle.odi.domain.project.finder.IOdiVariableFinder
 import oracle.odi.domain.runtime.loadplan.OdiLoadPlan
 import oracle.odi.domain.runtime.loadplan.finder.IOdiLoadPlanFinder
 import oracle.odi.domain.runtime.scenario.OdiScenario
 import oracle.odi.domain.runtime.scenario.finder.IOdiScenarioFinder
+import oracle.odi.domain.topology.OdiContext
+import oracle.odi.domain.topology.OdiDataServer
+import oracle.odi.domain.topology.OdiLogicalAgent
+import oracle.odi.domain.topology.OdiLogicalSchema
+import oracle.odi.domain.topology.OdiPhysicalAgent
+import oracle.odi.domain.topology.OdiPhysicalSchema
+import oracle.odi.domain.topology.OdiTechnology
+import oracle.odi.domain.topology.finder.IOdiContextFinder
+import oracle.odi.domain.topology.finder.IOdiDataServerFinder
+import oracle.odi.domain.topology.finder.IOdiLogicalAgentFinder
+import oracle.odi.domain.topology.finder.IOdiLogicalSchemaFinder
+import oracle.odi.domain.topology.finder.IOdiPhysicalAgentFinder
+import oracle.odi.domain.topology.finder.IOdiPhysicalSchemaFinder
+import oracle.odi.domain.topology.finder.IOdiTechnologyFinder
 
 @Slf4j
 class Instance {
@@ -97,11 +119,6 @@ class Instance {
 
    }
 
-   def getOdiProject(String projectCode) {
-
-      return findProject(projectCode)
-   }
-
    def beginTxn() {
 
       this.transaction = odi.getTransactionManager()
@@ -128,6 +145,11 @@ class Instance {
 
    def getProjectFinder() {
       return (IOdiProjectFinder) odi.getTransactionalEntityManager().getFinder(OdiProject.class)
+   }
+
+   def getOdiProject(String projectCode) {
+
+      return findProject(projectCode)
    }
 
    def findProject(String code, Boolean ignore = true) {
@@ -273,4 +295,167 @@ class Instance {
       log.info "Load plan list: $list"
       return list
    }
+
+   // Variable Finder
+   def getVariableFinder() {
+      def finder = (IOdiVariableFinder) odi.getTransactionalEntityManager().getFinder(OdiVariable.class)
+      return finder
+   }
+
+   def findAllGlobalVariables() {
+      def list = getVariableFinder().findAllGlobals()
+      log.info "Global Variables list: $list"
+      return list
+   }
+
+   def findVariable(String projectCode) {
+      def list = getVariableFinder().findByProject(projectCode)
+      log.info "Project Variables list: $list"
+      return list
+   }
+
+   // KM Finder
+   def getKMFinder() {
+      def finder = (IOdiKMFinder) odi.getTransactionalEntityManager().getFinder(OdiKM.class)
+      return finder
+   }
+
+   def findAllGlobalKMs() {
+      def list = getKMFinder().findAllGlobals()
+      log.info "Global KMs list: $list"
+      return list
+   }
+
+   def findKM(String projectCode) {
+      def list = getKMFinder().findByProject(projectCode)
+      log.info "KM list: $list"
+      return list
+   }
+
+   // Sequence Finder
+   def getSequenceFinder() {
+      def finder = (IOdiSequenceFinder) odi.getTransactionalEntityManager().getFinder(OdiSequence.class)
+      return finder
+   }
+
+   def findAllGlobalSequences() {
+      def list = getSequenceFinder().findAllGlobals()
+      log.info "Global Sequences list: $list"
+      return list
+   }
+
+   def findSequence(String projectCode) {
+      def list = getSequenceFinder().findByProject(projectCode)
+      log.info "Sequence list: $list"
+      return list
+   }
+
+   // UserFunction Finder
+   def getUserFunctionFinder() {
+      def finder = (IOdiUserFunctionFinder) odi.getTransactionalEntityManager().getFinder(OdiUserFunction.class)
+      return finder
+   }
+
+   def findAllGlobalUserFunctions() {
+      def list = getUserFunctionFinder().findAllGlobals()
+      log.info "UserFunction Sequences list: $list"
+      return list
+   }
+
+   def findUserFunction(String projectCode) {
+      def list = getUserFunctionFinder().findByProject(projectCode)
+      log.info "UserFunctionFinder list: $list"
+      return list
+   }
+
+   // Context Finder
+   def getContextFinder() {
+      def finder = (IOdiContextFinder) odi.getTransactionalEntityManager().getFinder(OdiContext.class)
+      return finder
+   }
+
+   def findAllContext() {
+      def list = getContextFinder().findAll()
+      log.info "Context list: $list"
+      return list
+   }
+
+   // Technology Finder
+   def getTechnologyFinder() {
+      def finder = (IOdiTechnologyFinder) odi.getTransactionalEntityManager().getFinder(OdiTechnology.class)
+      return finder
+   }
+
+   def findAllTechnology() {
+      def list = getTechnologyFinder().findAll()
+      log.info "Technology list: $list"
+      return list
+   }
+
+   def findUsedTechnologies() {
+      def list = getTechnologyFinder().findUsedTechnologies()
+      log.info "Technology list: $list"
+      return list
+   }
+
+   // DataServer Finder
+   def getDataServerFinder() {
+      def finder = (IOdiDataServerFinder) odi.getTransactionalEntityManager().getFinder(OdiDataServer.class)
+      return finder
+   }
+
+   def findAllDataServer() {
+      def list = getDataServerFinder().findAll()
+      log.info "DataServer list: $list"
+      return list
+   }
+
+   // PhysicalSchema Finder
+   def getPhysicalSchemaFinder() {
+      def finder = (IOdiPhysicalSchemaFinder) odi.getTransactionalEntityManager().getFinder(OdiPhysicalSchema.class)
+      return finder
+   }
+
+   def findAllPhysicalSchema() {
+      def list = getPhysicalSchemaFinder().findAll()
+      log.info "PhysicalSchema list: $list"
+      return list
+   }
+
+   // PhysicalAgent Finder
+   def getPhysicalAgentFinder() {
+      def finder = (IOdiPhysicalAgentFinder) odi.getTransactionalEntityManager().getFinder(OdiPhysicalAgent.class)
+      return finder
+   }
+
+   def findAllPhysicalAgent() {
+      def list = getPhysicalAgentFinder().findAll()
+      log.info "PhysicalAgent list: $list"
+      return list
+   }
+
+   // LogicalAgent Finder
+   def getLogicalAgentFinder() {
+      def finder = (IOdiLogicalAgentFinder) odi.getTransactionalEntityManager().getFinder(OdiLogicalAgent.class)
+      return finder
+   }
+
+   def findAllLogicalAgent() {
+      def list = getLogicalAgentFinder().findAll()
+      log.info "LogicalAgent list: $list"
+      return list
+   }
+
+   // LogicalSchema Finder
+   def getLogicalSchemaFinder() {
+      def finder = (IOdiLogicalSchemaFinder) odi.getTransactionalEntityManager().getFinder(OdiLogicalSchema.class)
+      return finder
+   }
+
+   def findAllLogicalSchema() {
+      def list = getLogicalSchemaFinder().findAll()
+      log.info "LogicalSchema list: $list"
+      return list
+   }
+
 }
