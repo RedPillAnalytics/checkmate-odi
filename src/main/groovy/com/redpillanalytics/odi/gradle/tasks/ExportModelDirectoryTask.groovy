1d2727f5cadb1c108fbc6308b89dc8e572e75833
josecsotomorales <josecsmorales@gmail.com>
2019-09-27 12:25:05 -0300

    Corrected ExportModelDirectoryTask

--- src/main/groovy/com/redpillanalytics/odi/gradle/tasks/ExportModelDirectoryTask.groovy
+++ src/main/groovy/com/redpillanalytics/odi/gradle/tasks/ExportModelDirectoryTask.groovy
@@ -21,17 +21,38 @@ class ExportModelDirectoryTask extends ExportDirectoryTask {
            description = "The ODI model code to export. Default: null, which means all models are exported.")
    String modelCode
 
+   /**
+    * The ODI model folder name to export. Default: null, which means all models folders are exported.
+    */
+   @Input
+   @Optional
+   @Option(option = "model-folder",
+           description = "The ODI model folder name to export. Default: null, which means all model folders are exported.")
+   String modelfolders
+
    @TaskAction
    def exportModelDirectory() {
 
       instance.connect()
+      // get the model folders
+      def modelfolders = modelFolderName ? instance.findModelFolderbyName(modelFolderName) : instance.findAllModelFolders()
+
+      // get the models
       def models = modelCode ? instance.findModelbyCode(modelCode) : instance.findAllModels()
 
       instance.beginTxn()
-      models.each {
-         exportObject(it, sourceBase.canonicalPath, true)
+      // export the model folders
+      modelfolders.each {
+         exportObject(it, "${sourceBase.canonicalPath}/model-folder", true)
          //smartExportObject(it, sourceBase.canonicalPath, it.name)
       }
+
+      // export the models
+      models.each {
+         exportObject(it, "${sourceBase.canonicalPath}/model", true)
+         //smartExportObject(it, sourceBase.canonicalPath, it.name)
+      }
+
       instance.endTxn()
    }
 }
