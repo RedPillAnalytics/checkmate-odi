65aca17acfb89c35c5ca4f73599f51be056ee5ba
josecsotomorales <josecsmorales@gmail.com>
2019-10-15 18:40:31 -0300

    Corrected ImportProjectDirectoryTask

--- src/main/groovy/com/redpillanalytics/odi/gradle/tasks/ImportProjectDirectoryTask.groovy
+++ src/main/groovy/com/redpillanalytics/odi/gradle/tasks/ImportProjectDirectoryTask.groovy
@@ -17,9 +17,9 @@ class ImportProjectDirectoryTask extends ImportDirectoryTask {
     * @return The List of export files.
     */
    @Internal
-   List getImportFiles() {
+   List getImportProjectFile() {
 
-      def filePrefix = ['PROJ', 'VAR', 'UFN', 'SEQ','FOLD','TRT','REUMAP','MAP','PACK']
+      def filePrefix = ['PROJ']
 
       def result = new LinkedList()
 
@@ -49,9 +49,32 @@ class ImportProjectDirectoryTask extends ImportDirectoryTask {
       return result
    }
 
+   /**
+    * Gets the hierarchical collection of XML files, sorted using folder structure and file name prefix logic.
+    *
+    * @return The List of export files.
+    */
+   @Internal
+   List getImportFiles() {
+
+      def filePrefix = ['VAR', 'UFN', 'SEQ','FOLD','TRT','REUMAP','MAP','PACK']
+
+      def result = new LinkedList()
+
+      filePrefix.each {
+         result.addAll(project.fileTree(dir: importDir, include: "**/${it}_*.xml").toList())
+      }
+
+      return result
+   }
+
    @TaskAction
    def taskAction() {
+      // Import the Project File
+      importXmlFiles(importProjectFile)
+      // Smart Import the Project KM
       smartImportXmlFiles(smartImportFiles)
+      // Import the Objects
       importXmlFiles(importFiles)
    }
 
