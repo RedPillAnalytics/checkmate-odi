3012d3038f73d3da649b881198817c3c004c9864
Jose Carlos Soto Morales <josecsmorales@gmail.com>
2020-04-29 14:47:31 -0300

    Added RollbackException logging to end transaction.

--- src/main/groovy/com/redpillanalytics/odi/odi/Instance.groovy
+++ src/main/groovy/com/redpillanalytics/odi/odi/Instance.groovy
@@ -71,6 +71,8 @@ import oracle.odi.domain.topology.finder.IOdiPhysicalAgentFinder
 import oracle.odi.domain.topology.finder.IOdiPhysicalSchemaFinder
 import oracle.odi.domain.topology.finder.IOdiTechnologyFinder
 
+import javax.persistence.RollbackException
+
 @Slf4j
 class Instance {
 
@@ -148,7 +150,7 @@ class Instance {
             odi.getTransactionManager().commit(this.transaction)
          }
 
-      } catch(Exception e) {
+      } catch(RollbackException e) {
          log.info("Transaction Rolled Back.")
          log.debug(e.toString())
       }
